046843a158bb5d939a054583100e93ed2b66a190
marrythomson70 <marrythomson70@gmail.com>
2024-11-19 19:55:24 +0530

    Create test_math.py

---
+++ test_math.py
@@ -0,0 +1,28 @@
+name: Hello World
+on:
+  push: 
+    branches:
+      - main
+jobs:
+  first-hello-world-job:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v3
+    - name: Print message
+      run: echo "Hello world GA Testing"
+run-pytest:
+  runs-on: ubuntu-latest
+ steps:
+- name: Checkout code
+  uses: actions/checkout@v3
+- name: Setup Python
+  uses: actions/setup-python@v3
+  with: 
+    python-version: 3.9
+- name: Install dependecies
+run: 
+  python -n pip install --upgrade pip 
+  pip install pytest
+- name: Run testcases
+  run: pytest
